8135c3f8b0a7adbca739651ee9ed763c6eb1e0d6
matheusmazzoni <matheusdiasmazzoni@gmail.com>
2019-09-26 16:28:39 -0300

    Adicionando comentarios
    Deixando link para a documentação do confluence no Main.java

--- src/main/java/br/eti/ns/Main.java
+++ src/main/java/br/eti/ns/Main.java
@@ -13,8 +13,8 @@ public class Main {
             - Aqui um exemplo de chamada de download de um unico documento atraves da chave
               (pode ser feito tanto pela chave do documento ou pelo NSU do mesmo):
 
-                * String resposta = DDFeAPI.downloadUnico("29988990000108", "/home/mazzoni/Notas", "1", "", "55", "43190800412417000161550010001139801717006581",
-                                    false, true, false);
+                * String resposta = DDFeAPI.downloadUnico(CNPJInteressado, caminho, tpAmb, nsu, modelo, chave,
+                                    incluirPdf, apenasComXml, comEventos);
 
 
             - Aqui um exemplo de chamada de download de lote de documentos
@@ -23,7 +23,7 @@ public class Main {
                 * String resposta = DDFeAPI.downloadLote(CNPJInteressado, caminho, tpAmb, ultNSU, modelo,
                                       apenasPendManif, incluirPdf, apenasComXml, comEventos);
 
-            Para maiores informações, consulte a documentação no link: https://confluence.ns.eti.br/display/PUB/PHP+-+DDF-e+API, e entre em contato com a equipe de integração
+            Para maiores informações, consulte a documentação no link: https://confluence.ns.eti.br/display/PUB/JAVA+-+DDF-e+API, e entre em contato com a equipe de integração
 
         */
 
